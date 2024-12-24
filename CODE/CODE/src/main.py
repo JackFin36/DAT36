2b325106a4a74a1554ab5ce487e9e6ed7c655f51
JackFin36 <jackfin.jf@gmail.com>
2024-12-24 09:51:01 +0100

    Delete CODE/CODE directory

--- CODE/CODE/src/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt6 import QtWidgets, uic, QtCore
-from gui.MainWindow import MainWindow
-      
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    main_window = MainWindow()
-    main_window.show()
-    sys.exit(app.exec())
